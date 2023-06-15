4e79333121418210d0f8d1109bba04ddf2608098
dream0544 <dream0544@cnspartner.com>
2023-06-15 17:29:30 +0900

    1. 이력서 탭적용

--- WebContent/v2/js/resume/resume.js
+++ WebContent/v2/js/resume/resume.js
@@ -1,21 +1,69 @@
 let quickWrapTop = document.querySelector('.quick-wrap').offsetTop;
 let anchorTarget = document.querySelectorAll('[id*=tabpanel]');
-let removeOn = (target) => [...target.parentElement.children].forEach((sibling) => { 
-  sibling != target && sibling.classList.remove('on') 
+
+function removeOn(target) {
+  [...target.parentElement.children].forEach((sibling) => {
+    sibling != target && sibling.firstElementChild.classList.remove('on')
+  })
+};
+
+function findSibling(target, className) {
+  let sibling = [...target.parentElement.children].find((sibling) => {
+    return sibling.classList.contains(className)
+  })
+  return sibling;
+};
+
+function indexOfItem(target) {
+  return [...document.querySelector('.quick-list').children].indexOf(target)
+}
+
+[...document.querySelectorAll('.collapse-btn')].forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    findSibling(e.target, 'section').classList.toggle('collapse');
+  })
 });
 
+document.querySelector('.quick-collapse-btn').addEventListener('click', function () {
+  document.querySelector('.quick-wrap').classList.toggle('collapse');
+  document.querySelector('.quick-list').style.transform = `translateY(0)`;
+})
+
+document.querySelectorAll('.quick-list .quick-item button').forEach((entry) => {
+  entry.addEventListener('click', function () {
+    let top = document.getElementById(`${entry.dataset.link}`).offsetTop - 45;
+    window.scrollTo({ top: top, behavior: 'smooth' });
+    document.querySelector('.quick-wrap').classList.add('collapse');
+
+  });
+})
+
 window.addEventListener('scroll', () => {
-  if (window.scrollY >= quickWrapTop) { document.querySelector('.quick-wrap').classList.add('on') }
-  else if (window.scrollY < quickWrapTop) { console.log('aaa'); document.querySelector('.quick-wrap').classList.remove('on') }
+  if (window.scrollY >= quickWrapTop) document.querySelector('.quick-wrap').classList.add('on')
+  else if (window.scrollY < quickWrapTop) document.querySelector('.quick-wrap').classList.remove('on')
 
   for (let target of anchorTarget) {
     if (window.scrollY < quickWrapTop) {
-      removeOn(document.querySelector('a[href="#section07"'))
+      removeOn(document.querySelector('button[data-link="section07"').parentElement)
     } else if (target.offsetHeight + target.offsetTop >= window.scrollY) {
-      let id = target.previousElementSibling.id; console.log(id);
-      removeOn(document.querySelector('a[href="#' + id + '"'))
-      document.querySelector('a[href="#' + id + '"').classList.add('on')
+      let targetPannel = findSibling(target, 'title');
+      let targetTab = document.querySelector(`button[data-link="${targetPannel.id}"`);
+      removeOn(targetTab.parentElement);
+      targetTab.classList.add('on');
+      let idx = Number(indexOfItem(targetTab.parentElement)) * 43;
+      if (window.innerWidth < 767) document.querySelector('.quick-list').style.transform = `translateY(-${idx}px)`;
       return;
     }
   }
 });
+
+const observer = new ResizeObserver(entries => {
+  for (let entry of entries) {
+    const { width } = entry.contentRect;
+    if (window.innerWidth >= 767) {
+      document.querySelector('.quick-list').style.transform = `translateY(0)`;
+    }
+  }
+})
+
+observer.observe(document.querySelector('.quick-list'), { box: 'content-box' });
