7736fcdf6bb661bc32d6b74fcb84b4c69a8c04eb
dream0544 <dream0544@cnspartner.com>
2024-01-24 18:04:41 +0900

    [메인] CSS작업 WebContent/v2/scss/common/_base.scss WebContent/v2/scss/common/_content.scss WebContent/v2/scss/common/_font.scss WebContent/v2/scss/common/_keyframes.scss WebContent/v2/scss/common/_utilites.scss WebContent/v2/scss/component/BN0001.scss WebContent/v2/scss/component/BN0002.scss WebContent/v2/scss/component/BN0003.scss WebContent/v2/css/common.min.css WebContent/v2/css/maps/common.min.css.map WebContent/index.jsp WebContent/v2/scss/component/LS0001.scss WebContent/v2/scss/component/LS0002.scss WebContent/v2/css/main.min.css WebContent/v2/css/maps/main.min.css.map WebContent/v2/scss/main.scss WebContent/v2/js/component/NT0001.js WebContent/v2/css/maps/portfolio.min.css.map WebContent/v2/css/maps/resume.min.css.map WebContent/v2/font/SeoulNamsanM.woff WebContent/v2/scss/component/TX0001.scss WebContent/v2/scss/component/TX0002.scss WebContent/v2/scss/component/TX0003.scss WebContent/WEB-INF/web.xml

--- WebContent/v2/js/component/NT0001.js
+++ WebContent/v2/js/component/NT0001.js
@@ -1,61 +1,75 @@
 //notice
+const tx0001 = document.querySelector('.TX0001');
 if(sessionStorage.getItem('top-notice-off') === null){
-	document.querySelector('.TX0001').classList.remove('hidden');
+	tx0001.classList.remove('hidden');
 }else{
-	document.querySelector('.TX0001').classList.add('hidden');
+	tx0001.classList.add('hidden');
 }
 
-document.querySelector('.right-gutter').addEventListener('click', function(){
+tx0001.querySelector('.c-button').addEventListener('click', function(){
     sessionStorage.setItem('top-notice-off', true);
-    document.querySelector('.right-gutter').parentElement.classList.add('hidden');
+    tx0001.querySelector('.c-button').parentElement.classList.add('hidden');
 })
 
 setInterval(()=>{
-   if(document.querySelector('.TX0001 .c-rolling__txt.active').nextElementSibling === null){
-       document.querySelector('.TX0001 .c-rolling__txt.active').classList.remove('active');
-        document.querySelector('.TX0001 .c-rolling__txt').classList.add('active');
+   if(tx0001.querySelector('.c-rolling__txt.active').nextElementSibling === null){
+       tx0001.querySelector('.c-rolling__txt.active').classList.remove('active');
+        tx0001.querySelector('.c-rolling__txt').classList.add('active');
    } else {
-       document.querySelector('.TX0001 .c-rolling__txt.active').nextElementSibling.classList.add('active');
-        document.querySelector('.TX0001 .c-rolling__txt.active').classList.remove('active');
+       tx0001.querySelector('.c-rolling__txt.active').nextElementSibling.classList.add('active');
+       tx0001.querySelector('.c-rolling__txt.active').classList.remove('active');
    }
 },4000);
 //--notice
 
 //scroll event
 let offset =0;
+const body = document.querySelector('body');
+const bn0001 = document.querySelector('.BN0001');
+const bn0002 = document.querySelector('.BN0002');
+const bn0003 = document.querySelector('.BN0003');
+const ls0001 = document.querySelector('.LS0001');
+const ls0002 = document.querySelector('.LS0002');
+const tx0002 = document.querySelector('.TX0002');
 function scrollEvt() {
     offset = window.scrollY;
-    if(offset > document.querySelector('.BN0002').offsetTop - 300){
-        document.querySelector('.BN0001').classList.add('opacity-0');
-        document.querySelector('.BN0002').classList.add('active');
-        document.querySelector('body').classList.add('bg-beige');                
+    if(offset > bn0002.offsetTop - 300){
+        bn0001.classList.add('opacity-0');
+        bn0002.classList.add('active');
+        body.classList.add('bg-beige');                
     }else{
-        document.querySelector('.BN0001').classList.remove('opacity-0');
-        document.querySelector('.BN0002').classList.remove('active');
-        document.querySelector('body').classList.remove('bg-beige');
+        bn0001.classList.remove('opacity-0');
+        bn0002.classList.remove('active');
+        body.classList.remove('bg-beige');
     }
     
-    if(offset > document.querySelector('.LS0001').offsetTop - 300){
-        document.querySelector('.LS0001').classList.add('active');
-        document.querySelector('body').classList.add('bg-white');
+    if(offset > ls0001.offsetTop - 300){
+        ls0001.classList.add('active');
+        body.classList.add('bg-white');
     }else{
-		document.querySelector('.LS0001').classList.remove('active');
-		document.querySelector('body').classList.remove('bg-white');
+		ls0001.classList.remove('active');
+		body.classList.remove('bg-white');
 	}
     
-    if(offset > document.querySelector('.BN0003').offsetTop - 300){
-        document.querySelector('.BN0003').classList.add('active');
+    if(offset > bn0003.offsetTop - 300){
+        bn0003.classList.add('active');
     }else{
-		document.querySelector('.BN0003').classList.remove('active');
+		bn0003.classList.remove('active');
 	}
 	
-	if(offset > document.querySelector('.TX0002').offsetTop - 300){
-        document.querySelector('.TX0002').classList.add('active');
+	if(offset > tx0002.offsetTop - 300){
+        tx0002.classList.add('active');
     }else{
-		document.querySelector('.TX0002').classList.remove('active');
+		tx0002.classList.remove('active');
 	}
 	
-    document.querySelector('.TX0002 .c-floating-contents__floor').style.top = (document.querySelector('.TX0002').offsetTop - offset - 200 ) * 1.5  + 'px';
+	if(offset > ls0002.offsetTop - 300){
+        ls0002.classList.add('active');
+    }else{
+		ls0002.classList.remove('active');
+	}
+	
+    tx0002.querySelector('.c-floating-contents__floor').style.top = (tx0002.offsetTop - offset - 200 ) * 1.5  + 'px';
 }
 window.addEventListener('scroll', scrollEvt );
 //--scroll event
